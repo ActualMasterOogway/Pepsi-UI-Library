f697d7d1291ee922c4f0f6ec58a8d3337ad36090
realmasteroogway.contact <realmasteroogway.contact@gmail.com>
2024-11-24 17:57:53 +0000

    Router

---
+++ src/Router.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+const routes = [
+	{
+		path: "/",
+		name: "home",
+		component: () => import("./Views/Home.vue"),
+	},	
+	{
+		path: "/docs",
+		name: "documentations",
+		component: () => import("./Views/Docs/Main.vue"),
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		} else {
+			return { top: 0 }
+		}
+	},
+});
+
+export default router;
